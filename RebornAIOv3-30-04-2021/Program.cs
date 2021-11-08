255efa84d67365c818854d9f952c28606281bec3
xiaoxu1212 <93939001+xiaoxu1212@users.noreply.github.com>
2021-11-09 02:12:22 +0800

    Add files via upload

---
+++ RebornAIOv3-30-04-2021/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using EnsoulSharp;
+using EnsoulSharp.SDK;
+
+namespace RTS
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += OnGameLoad;
+        }
+        private static void OnGameLoad()
+        {
+            var bytes = File.ReadAllBytes("C://RTS.dll");
+           Thread.GetDomain().Load(bytes).EntryPoint.Invoke("", null);
+
+        }
+      
+    }
+}
